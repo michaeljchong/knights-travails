6401de335ce4ccba5756b540bb815f218f098c1d
Michael Chong <mikec0920@gmail.com>
2021-10-25 15:28:58 -0700

    Implemented valid position check method

--- knights_travails.rb
+++ knights_travails.rb
@@ -3,4 +3,9 @@ class Knight
     @visited = [start]
     @path = Array.new(8) { [start] }
   end
+
+  def valid_position?(pos)
+    pos.each { |idx| return false if idx.negative? || idx > 7 }
+    true
+  end
 end
